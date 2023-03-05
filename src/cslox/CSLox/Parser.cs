8a406f59b1a85cf55dcffd54b71b719e0663f74b
EngineerFergus <williamskei97@gmail.com>
2023-03-05 11:05:20 -0500

    [Chapter 9.5.1, pg 148] Got for loops working. Wrote a script to test it.

--- src/cslox/CSLox/Parser.cs
+++ src/cslox/CSLox/Parser.cs
@@ -52,6 +52,11 @@ namespace CSLox
 
         private Stmt Statement()
         {
+            if (Match(TokenType.FOR))
+            {
+                return ForStatement();
+            }
+
             if (Match(TokenType.IF))
             {
                 return IfStatement();
@@ -73,6 +78,70 @@ namespace CSLox
             }
 
             return ExpressionStatement();
+        }
+
+        private Stmt ForStatement()
+        {
+            Consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.");
+
+            Stmt? initializer;
+
+            if (Match(TokenType.SEMICOLON))
+            {
+                initializer = null;
+            }
+            else if (Match(TokenType.VAR))
+            {
+                initializer = VarDeclaration();
+            }
+            else
+            {
+                initializer = ExpressionStatement();
+            }
+
+            Expr? condition = null;
+            if (!Check(TokenType.SEMICOLON))
+            {
+                condition = Expression();
+            }
+            Consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+            Expr? increment = null;
+            if (!Check(TokenType.RIGHT_PAREN))
+            {
+                increment = Expression();
+            }
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.");
+            Stmt body = Statement();
+
+            if (increment != null)
+            {
+                body = new Stmt.Block(
+                    new List<Stmt?>()
+                    {
+                        body,
+                        new Stmt.Expression(increment)
+                    });
+            }
+
+            if (condition == null)
+            {
+                condition = new Expr.Literal(true);
+            }
+
+            body = new Stmt.WhileLoop(condition, body);
+
+            if (initializer != null)
+            {
+                body = new Stmt.Block(
+                    new List<Stmt?>() 
+                    {
+                        initializer,
+                        body
+                    });
+            }
+
+            return body;
         }
 
         private Stmt IfStatement()
